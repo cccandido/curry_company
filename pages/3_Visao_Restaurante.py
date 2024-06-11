2fc21e7b80c496a9ed04f04cba90974e5fcfa165
Camila Candido <139182303+cccandido@users.noreply.github.com>
2024-06-11 09:20:22 -0400

    Update 3_Visao_Restaurante.py

--- pages/3_Visao_Restaurante.py
+++ pages/3_Visao_Restaurante.py
@@ -80,9 +80,9 @@ st.sidebar.markdown("""___""")
 st.sidebar.markdown('## Selecione uma data limite')
 date_slider=st.sidebar.slider (
     'Data limite',
-    value=pd.datetime( '2022', '4', '13' ),
-    min_value = pd.datetime( '2022', '2', '11' ),
-    max_value = pd.datetime( '2022', '4', '6' ),
+    value=pd.datetime( 2022, 4, 13 ),
+    min_value = pd.datetime( 2022, 2, 11 ),
+    max_value = pd.datetime( 2022, 4, 6 ),
     format = 'DD-MM-YYYY' )
     
 st.sidebar.markdown ( """___""" )
