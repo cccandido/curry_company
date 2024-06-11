590eae5a81bd59291ed5e2af10f7d075b0deb216
Camila Candido <139182303+cccandido@users.noreply.github.com>
2024-06-11 09:24:00 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,15 +16,14 @@ st.write ('# Curry Company Growth Dashboard')
 
 st.markdown (
     """
-    Growth Dashboard foi construido para acompanhar as métricas de crescimento dos Entregadores e dos Restauranes.
-    ### Como utilizar esse Growth Dashboard?
+    Growth Dashboard foi construido para acompanhar as métricas de crescimento dos Entregadores e dos Restaurantes.
+    ### Como o Dashboard foi construído?
     - Visão empresa:
         - Visão gerencial: métricas gerais de comportamento
         - Visão tática: indicadores semais de crescimento
         - Visão geográfica: insights de geolocalização
     - Visõ entregador: acompanhamento dos indicadores semanais de crescimento
     - Visãorestaurante: indicadores semanais de crescimento dos restaurantes
-    ### Ask for Help
-    - Time de Data Science no Discord 
+    
     """ 
     )
